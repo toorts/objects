ef72f1301e177b2ab30ee68c288a1b80d4405280
Martin House <torchester@gmail.com>
2023-03-27 16:12:19 +0500

    add task 4

---
+++ src/gbLinkedList/GBLinkedList.java
@@ -0,0 +1,62 @@
+package gbLinkedList;
+
+public class GBLinkedList<T> {
+    private Node<T> head;
+    private Node<T> tail;
+    private int size;
+
+    private static class Node<T> {
+        private T data;
+        private Node<T> next;
+
+        public Node(T data) {
+            this.data = data;
+        }
+    }
+
+    public GBLinkedList() {
+        this.head = null;
+        this.tail = null;
+        this.size = 0;
+    }
+
+    public void addToFront(T data) {
+        Node<T> newNode = new Node<>(data);
+        if (head == null) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            newNode.next = head;
+            head = newNode;
+        }
+        size++;
+    }
+
+    public void addToEnd(T data) {
+        Node<T> newNode = new Node<>(data);
+        if (tail == null) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            tail.next = newNode;
+            tail = newNode;
+        }
+        size++;
+    }
+
+    public T get(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Index " + index + " is out of bounds!");
+        }
+
+        Node<T> current = head;
+        for (int i = 0; i < index; i++) {
+            current = current.next;
+        }
+        return current.data;
+    }
+
+    public int size() {
+        return size;
+    }
+}
---
+++ src/gbLinkedList/Main.java
@@ -0,0 +1,20 @@
+package gbLinkedList;
+
+public class Main {
+    public static void main(String[] args) {
+        GBLinkedList<String> list = new GBLinkedList<>();
+
+        // Добавление элементов в начало и конец списка
+        list.addToFront("world");
+        list.addToFront("hello");
+        list.addToEnd("!");
+
+        // Получение размера списка
+        System.out.println("Size of the list: " + list.size());
+
+        // Получение элементов по индексу
+        System.out.println("Element at index 0: " + list.get(0));
+        System.out.println("Element at index 1: " + list.get(1));
+        System.out.println("Element at index 2: " + list.get(2));
+    }
+}
